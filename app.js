6ec4633829dba10b85a4aa7f20fc9910137ecd4c
Kason <kasonjvoorhees@gmail.com>
2022-05-24 12:12:47 -0700

    removed previous lines

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 
 const recipes = require("./routes/recipes");
 const pageControl = require("./routes/control");
@@ -8,20 +7,12 @@ const pageControl = require("./routes/control");
 require("dotenv").config();
 const CONNECT_DB = require("./DB/connect");
 
-// console.log(path);
-const buildPath = path.join(__dirname, "../", "client", "build");
-
-app.use(express.static(buildPath));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   // this will be the homepage of the app.
   res.send("home page");
 });
-app.get("*"),
-  (req, res) => {
-    res.sendFile(path.join(buildPath, "index.html"));
-  };
 
 app.use("/api", recipes);
 app.use("/api/page-control", pageControl);
